e0878eeb77793465fce5077c5a368cbc851c7a20
l1nes8 <egor1glavatyje@gmail.com>
2023-11-12 23:56:00 +0200

    Update 1.ts

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -4,7 +4,7 @@ let age: number = 50;
 let name: string = "Max";
 let toggle: boolean = true;
 let empty: null = null;
-let notInitialize: any = {};
+let notInitialize: unknown = 100;
 let callback: (a: number) => number = (a) => {
   return 100 + a;
 };
